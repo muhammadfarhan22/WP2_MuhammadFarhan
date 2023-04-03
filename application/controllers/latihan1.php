9e5dff89f923c9a91368a5f0061839c474e914d2
muhammadfarhan22 <muhammad.farhan22611@gmail.com>
2023-04-03 11:40:33 +0700

    nambah :)

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -3,7 +3,7 @@ class latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "Selamat Datang... selamat belajar web programming 2";
+        echo "Selamat Datang... selamat belajar web programming";
         // $this->load->view('view-latihan1');
     }
 
