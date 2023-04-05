dfe6d424fc6f6de96569f4d4e5d04478f910e3f7
muhammadfarhan22 <muhammad.farhan22611@gmail.com>
2023-04-05 14:13:39 +0700

    nambah lagi :)

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <img src="<?php echo base_url() ?>assets/img/avataaars.png" width="50px">
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Imam</li>
+        <li>Nama Belakang : Nawawi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
---
+++ application/views/v_hubungikami.php
@@ -0,0 +1,14 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Social Media</h4>
+    <ul type="disc">
+        <li>Facebook : huhuyao1</li>
+        <li>Instagram : browok12</li>
+    </ul>
+
+    <h4>Call Center</h4>
+    <ul type="disc">
+        <li>Phone : 081904203245</li>
+        <li>WA : 189247198479817</li>
+    </ul>
+</section>
